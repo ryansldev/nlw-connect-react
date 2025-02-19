aca152ca0755cec44af53248cd513fb9ad1a6883
ryansldev <ryanlima1309@gmail.com>
2025-02-19 11:01:23 -0300

    ✨ created input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import { ArrowRight } from "lucide-react"
+import { ArrowRight, Copy, Mail } from "lucide-react"
 import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
 
 export default function Home() {
   return (
@@ -8,6 +10,20 @@ export default function Home() {
         Enviar
         <ArrowRight />
       </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+       <InputRoot>
+        <InputIcon>
+          <Mail className="size-5" />
+        </InputIcon>
+
+        <InputField />
+       </InputRoot>
+      </div>
     </main>
   );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,12 @@
-import type { ReactNode } from "react";
+import type { ComponentProps } from "react";
 
-interface ButtonProps {
-  children: ReactNode;
-}
+interface ButtonProps extends ComponentProps<"button"> {}
 
-export function Button({ children }: ButtonProps) {
+export function Button(props: ButtonProps) {
   return (
-    <button className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
-      {children}
-    </button>
+    <button
+      className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      {...props}
+    />
   )
 }
