a932ddae6fc9de047ff1f87699fff2c329f224fb
ryansldev <ryanlima1309@gmail.com>
2025-02-19 00:02:10 -0300

    ✨ created button component

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { ArrowRight } from "lucide-react"
+import { Button } from "@/components/button";
+
+export default function Home() {
+  return (
+    <main>
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
+    </main>
+  );
+}
---
+++ src/components/button.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+}
+
+export function Button({ children }: ButtonProps) {
+  return (
+    <button className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
+      {children}
+    </button>
+  )
+}
